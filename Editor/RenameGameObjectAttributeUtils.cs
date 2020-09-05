04e1cab9511a0a95c15c7437daf025fda42709a0
baba_s <shotababa1206@gmail.com>
2020-09-05 15:40:36 +0900

    first commit

---
+++ Editor/RenameGameObjectAttributeUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	[InitializeOnLoad]
+	internal static class RenameGameObjectAttributeUtils
+	{
+		private static readonly Type ATTRIBUTE_TYPE = typeof( RenameGameObjectAttribute );
+
+		static RenameGameObjectAttributeUtils()
+		{
+			ObjectFactory.componentWasAdded += OnComponentWasAdded;
+		}
+
+		private static void OnComponentWasAdded( Component component )
+		{
+			Rename( component );
+		}
+
+		private static void Rename( Component component )
+		{
+			var type       = component.GetType();
+			var attributes = type.GetCustomAttributes( ATTRIBUTE_TYPE, true );
+
+			if ( attributes.Length <= 0 ) return;
+
+			var name       = type.Name;
+			var gameObject = component.gameObject;
+
+			Undo.RecordObject( gameObject, "Rename" );
+
+			gameObject.name = name;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# UniRenameGameObjectAttribute
